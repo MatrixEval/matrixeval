4d378a2503c925418a3052085a0abb2cbe5aaee8
Hopper Gee <hopper.gee@hey.com>
2022-02-07 17:56:10 +0800

    Refactor Matrixeval::Ruby::Init to call Config.create

--- lib/matrixeval/ruby/init.rb
+++ lib/matrixeval/ruby/init.rb
@@ -1,6 +1,7 @@
 module Matrixeval
   module Ruby
     class Init
+
       class << self
         def call
           new.call
@@ -8,32 +9,9 @@ module Matrixeval
       end
 
       def call
-        create_config_yaml
+        Config.create
       end
 
-      private
-
-      def create_config_yaml
-        return if File.exist?(config)
-
-        FileUtils.cp(template, config)
-      end
-
-      def template
-        gem_root_path.join("lib/matrixeval/ruby/templates/matrixeval.yml")
-      end
-
-      def config
-        working_dir.join("matrixeval.yml")
-      end
-
-      def working_dir
-        Pathname.new(Dir.getwd)
-      end
-
-      def gem_root_path
-        Matrixeval::Ruby.root
-      end
     end
   end
 end
--- test/matrixeval/ruby/init_test.rb
+++ test/matrixeval/ruby/init_test.rb
@@ -4,27 +4,10 @@ require "test_helper"
 
 class Matrixeval::Ruby::InitTest < MatrixevalTest
 
-  def setup
-    FileUtils.rm(dummy_gem_matrixeval_file_path) rescue nil
-    Matrixeval::Ruby::Init.any_instance.stubs(:working_dir).returns(dummy_gem_working_dir)
-  end
-
-  def test_creating_dot_matrixeval_yaml
-    refute File.exist?(dummy_gem_matrixeval_file_path)
+  def test_call_config_create
+    Matrixeval::Ruby::Config.expects(:create)
 
     Matrixeval::Ruby::Init.call
-
-    assert File.exist?(dummy_gem_matrixeval_file_path)
-  end
-
-  def test_not_delete_exising_dot_matrixeval_yaml
-    File.open(dummy_gem_matrixeval_file_path, "w+") do |file|
-      file.puts "Customizations"
-    end
-
-    Matrixeval::Ruby::Init.call
-
-    assert_equal "Customizations\n", File.read(dummy_gem_matrixeval_file_path)
   end
 
 end
