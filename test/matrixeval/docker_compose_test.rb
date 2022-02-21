5a78bd330f242a16c572c141f0e89a35d1cc4b6b
Hopper Gee <hopper.gee@hey.com>
2022-02-21 23:05:44 +0800

    Refactor Ruby::DockerCompose to DockerCompose

--- test/matrixeval/docker_compose_test.rb
+++ test/matrixeval/docker_compose_test.rb
@@ -2,47 +2,47 @@
 
 require "test_helper"
 
-class Matrixeval::Ruby::DockerComposeTest < MatrixevalTest
+class Matrixeval::DockerComposeTest < MatrixevalTest
 
   def test_context
     context = stub
-    docker_compose = Matrixeval::Ruby::DockerCompose.new(context)
+    docker_compose = Matrixeval::DockerCompose.new(context)
     assert_equal context, docker_compose.context
   end
 
   def test_run
-    Matrixeval::Ruby::Config::YAML.stubs(:yaml).returns({"project_name" => "dummy app"})
+    Matrixeval::Config::YAML.stubs(:yaml).returns({"project_name" => "dummy app"})
 
-    context = Matrixeval::Ruby::Context.new(
-      main_variant: Matrixeval::Ruby::Variant.new(
+    context = Matrixeval::Context.new(
+      main_variant: Matrixeval::Variant.new(
         {
           "key" => "3.0",
           "image" => "ruby:3.0.0"
         },
-        Matrixeval::Ruby::Vector.new("ruby", {"key" => "ruby"})
+        Matrixeval::Vector.new("ruby", {"key" => "ruby"})
       ),
       rest_variants: [
-        Matrixeval::Ruby::Variant.new(
+        Matrixeval::Variant.new(
           {
             "key" => "6.0",
             "env" => {
               "RAILS_VERSION" => "6.0.0"
             }
           },
-          Matrixeval::Ruby::Vector.new("rails", {"key" => "rails"})
+          Matrixeval::Vector.new("rails", {"key" => "rails"})
         ),
-        Matrixeval::Ruby::Variant.new(
+        Matrixeval::Variant.new(
           {
             "key" => "5.0",
             "env" => {
               "SIDEKIQ_VERSION" => "5.0.0"
             }
           },
-          Matrixeval::Ruby::Vector.new("sidekiq", {"key" => "sidekiq"})
+          Matrixeval::Vector.new("sidekiq", {"key" => "sidekiq"})
         ),
       ]
     )
-    docker_compose = Matrixeval::Ruby::DockerCompose.new(context)
+    docker_compose = Matrixeval::DockerCompose.new(context)
 
     docker_compose.expects(:system).with(<<~COMMAND
       docker --log-level error compose \
