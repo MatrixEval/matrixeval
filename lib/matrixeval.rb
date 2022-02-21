7f019af0eabc5cf9a3e5e77187fddf90a2ad707c
Hopper Gee <hopper.gee@hey.com>
2022-02-21 21:52:22 +0800

    Put most reqiure in matrixeval.rb

--- lib/matrixeval.rb
+++ lib/matrixeval.rb
@@ -1,22 +1,51 @@
 # frozen_string_literal: true
 
 require_relative "matrixeval/version"
-require 'rainbow'
-require 'matrixeval/docker_compose'
+
+require 'pathname'
+require 'fileutils'
+require 'optparse'
+
+require 'matrixeval/docker_compose/extend_raw'
+require 'matrixeval/docker_compose/extend'
+require 'matrixeval/container'
+require 'matrixeval/variant'
+require 'matrixeval/vector'
+require 'matrixeval/config'
+
+require 'matrixeval/target'
+
 require 'matrixeval/context'
 require 'matrixeval/extra_mount_files'
-require 'matrixeval/runner'
+require 'matrixeval/docker_compose'
 require 'matrixeval/gitignore'
+
+require 'matrixeval/command_line'
+require 'matrixeval/runner'
 
 module Matrixeval
 
+  class Error < StandardError; end
+
   module_function
   def start(argv)
-    Ruby::Runner.start(argv)
+    Runner.start(argv)
+  end
+
+  def register_target(target_name, target_klass)
+    targets[target_name] = target_klass
+  end
+
+  def targets
+    @targets ||= {}
   end
 
   def working_dir
     Pathname.new(Dir.getwd)
+  end
+
+  def root
+    Pathname.new("#{__dir__}/..")
   end
 
 end
