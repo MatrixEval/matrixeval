845b99bf40e6d7c603109e6d62cdbd920e9603b7
Hopper Gee <hopper.gee@hey.com>
2022-02-21 22:55:14 +0800

    Refactor Ruby::Context::BuildDockerComposeExtend to Context::BuildDockerComposeExtend

--- test/matrixeval/context/build_docker_compose_extend_test.rb
+++ test/matrixeval/context/build_docker_compose_extend_test.rb
@@ -2,10 +2,10 @@
 
 require 'test_helper'
 
-class Matrixeval::Ruby::Context::BuildDockerComposeExtendTest < MatrixevalTest
+class Matrixeval::Context::BuildDockerComposeExtendTest < MatrixevalTest
 
   def setup
-    Matrixeval::Ruby::Config::YAML.stubs(:yaml).returns({
+    Matrixeval::Config::YAML.stubs(:yaml).returns({
       "docker-compose-extend" => {
         "volumes" => {
           "postgres12-<%= matrix_combination_id %>" => nil
@@ -13,24 +13,24 @@ class Matrixeval::Ruby::Context::BuildDockerComposeExtendTest < MatrixevalTest
       }
     })
 
-    @ruby_vector = Matrixeval::Ruby::Vector.new("ruby", {"key" => "ruby"})
-    @sidekiq_vector = Matrixeval::Ruby::Vector.new("sidekiq", {"key" => "sidekiq"})
-    @rails_vector = Matrixeval::Ruby::Vector.new("rails", {"key" => "rails"})
+    @ruby_vector = Matrixeval::Vector.new("ruby", {"key" => "ruby"})
+    @sidekiq_vector = Matrixeval::Vector.new("sidekiq", {"key" => "sidekiq"})
+    @rails_vector = Matrixeval::Vector.new("rails", {"key" => "rails"})
 
-    @ruby_3_variant = Matrixeval::Ruby::Variant.new({"key" => "3.0"}, @ruby_vector)
-    @sidekiq_5_variant = Matrixeval::Ruby::Variant.new(
+    @ruby_3_variant = Matrixeval::Variant.new({"key" => "3.0"}, @ruby_vector)
+    @sidekiq_5_variant = Matrixeval::Variant.new(
       {
         "key" => "5.0",
         "env" => {"SIDEKIQ_VERSION" => "5.0.0"}
       }, @sidekiq_vector)
 
-    @rails_6_variant = Matrixeval::Ruby::Variant.new(
+    @rails_6_variant = Matrixeval::Variant.new(
       {
         "key" => "6.1",
         "env" => {"RAILS_VERSION" => "6.1.0"}
       }, @rails_vector)
 
-    @context = Matrixeval::Ruby::Context.new(
+    @context = Matrixeval::Context.new(
       main_variant: @ruby_3_variant,
       rest_variants: [@sidekiq_5_variant, @rails_6_variant]
     )
@@ -38,7 +38,7 @@ class Matrixeval::Ruby::Context::BuildDockerComposeExtendTest < MatrixevalTest
 
   def test_call
     compose_extend = @context.docker_compose_extend
-    assert compose_extend.is_a?(Matrixeval::Ruby::DockerCompose::Extend)
+    assert compose_extend.is_a?(Matrixeval::DockerCompose::Extend)
     assert_equal({ "postgres12-ruby_3_0_rails_6_1_sidekiq_5_0" => nil }, compose_extend.volumes)
   end
 
