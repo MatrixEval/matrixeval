f2b15af6343aa933f8db359ff2f7b1a5bcfe68ce
Hopper Gee <hopper.gee@hey.com>
2022-02-21 22:25:48 +0800

    Add Target.vector_key

--- lib/matrixeval/target.rb
+++ lib/matrixeval/target.rb
@@ -4,6 +4,10 @@ module Matrixeval
       def matrixeval_yml_template_path
         Matrixeval.root.join("lib/matrixeval/templates/matrixeval.yml")
       end
+
+      def vector_key
+        nil
+      end
     end
   end
 end
--- test/matrixeval/target_test.rb
+++ test/matrixeval/target_test.rb
@@ -6,4 +6,8 @@ class Matrixeval::TargetTest < MatrixevalTest
     assert_equal working_dir.join("lib/../lib/matrixeval/templates/matrixeval.yml"), Matrixeval::Target.matrixeval_yml_template_path
   end
 
+  def test_vector_key
+    assert Matrixeval::Target.vector_key.nil?
+  end
+
 end
